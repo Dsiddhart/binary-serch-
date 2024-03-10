4492afc2f7ff5c72cf00728effb76cc8c9777554
Dsidd <dsiddharth4295@gmail.com>
2024-03-10 15:58:35 +0530

    new

---
+++ sheet1.cpp
@@ -0,0 +1,18 @@
+    
+    
+    
+    int res =-1;
+    int start=0;
+    int end= nums.size()-1;
+    while(start<=end){
+        int mid= start + ((end-start)/2);
+        if(nums[mid]==target){
+            res=mid;
+            return res;
+        } else if (nums[mid] > target) {
+            end = mid - 1;
+        } else{
+            start=mid+1;
+        }
+    }
+    return res;
